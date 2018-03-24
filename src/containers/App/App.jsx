ddc4f891527bc6940cf02c37e0d8b3adbd0c52db
Dragos Nedelcu <dragosnedelcu@MacBook-Pro-de-Dragos.local>
2018-03-24 18:55:19 +0100

    removed footer

--- src/containers/App/App.jsx
+++ src/containers/App/App.jsx
@@ -125,7 +125,6 @@ class App extends Component {
                                     })
                                 }
                             </Switch>
-                        <Footer />
                     </div>
                 </div>
         );
